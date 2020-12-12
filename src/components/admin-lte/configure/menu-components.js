6da96245ed45c04109f2432a5b5e536182d9fbca
Chris Troutner <chris.troutner@gmail.com>
2020-12-12 08:04:06 -0800

    Forked from gatsby-ipfs-web-wallet

---
+++ src/components/admin-lte/configure/menu-components.js
@@ -0,0 +1,13 @@
+// const MenuComponents = () => {}
+
+// export default MenuComponents
+
+import React from 'react'
+
+export default [
+  {
+    key: 'Some key',
+    icon: 'fas-message',
+    component: <><p>test</p></>
+  }
+]
